23ba715e8c6f1ce5b160e4005355f47cb615af16
omerffkoc <kocfomer@gmail.com>
2023-04-03 03:18:36 +0300

    rastgelelik
    1-100 arası 6 sayı

--- deneme.cpp
+++ deneme.cpp
@@ -5,4 +5,14 @@ int main()
 {
 	cout<<"Hello world"<<endl; //Berat tarafından eklendi
 	cout << "Merhaba Dunya!"; //ömer tarafından eklendi
+	//1 - 100 arasında rastgele 6 sayı
+	int sayi = 1;
+	while (sayi < 7)
+	{
+		cout << "Sayilar: " << rand() % 100<< " " << endl;
+		sayi=sayi+1;
+
+}
+
+
 }
